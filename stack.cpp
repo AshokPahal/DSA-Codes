375c851c9147a7ec97b4a445e5986901050b379b
AshokPahal <99093716+AshokPahal@users.noreply.github.com>
2023-01-13 21:35:01 +0530

    stackSTL

--- stack.cpp
+++ stack.cpp
@@ -4,10 +4,26 @@ using namespace std;
 
 int main(){
 
-    stack<char> s;
+    stack<int> s;
 
-    for(int i=0; i<5; i++){
-        char ch = 
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+
+    s.pop();
+
+    cout<<" Top Element : "<<s.top()<<endl;
+
+    s.pop();
+    s.pop();
+    s.pop();
+
+    if(s.empty()){
+        cout<<"stack is empry"<<endl;
+    }
+    else{
+        cout<<"stack not empty"<<endl;
     }
 
     return 0;
