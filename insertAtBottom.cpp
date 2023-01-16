67706ab368096c0f8c526839159a7496fe874699
AshokPahal <99093716+AshokPahal@users.noreply.github.com>
2023-01-16 23:22:55 +0530

    Stack DSA

---
+++ insertAtBottom.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+void atbottom(stack<int>& s, int x){
+    if(s.empty()){
+        s.push(x);
+        cout<<s.top()<<" ";
+        return;
+    }
+
+    int num = s.top();
+    s.pop();
+    atbottom(s,x);
+    s.push(num);
+    cout<<s.top()<<" ";
+}
+
+int main(){
+    stack<int> s;
+    s.push(5);
+    s.push(4);
+    s.push(3);
+    s.push(2);
+
+    // int x;
+    // cin>>x;
+    atbottom(s,6);
+
+    return 0;
+}
